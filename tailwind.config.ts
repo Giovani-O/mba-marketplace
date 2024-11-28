743ac07caf5e71268625e6f0a0f7f37ba81cda3d
Giovani <giovani98@proton.me>
2024-11-27 21:28:03 -0300

    feat: Initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,45 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Poppins', 'sans-serif'],
+        heading: ['DM Sans', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        orange: {
+          base: 'var(--orange-base)',
+          dark: 'var(--orange-dark)',
+        },
+        blue: {
+          light: 'var(--blue-light)',
+          base: 'var(--blue-base)',
+          dark: 'var(--blue-dark)',
+        },
+        shape: {
+          white: 'var(--white)',
+          background: 'var(--background)',
+          default: 'var(--shape)', // 'default' as an alias for 'shape'
+        },
+        gray: {
+          100: 'var(--gray-100)',
+          200: 'var(--gray-200)',
+          300: 'var(--gray-300)',
+          400: 'var(--gray-400)',
+          500: 'var(--gray-500)',
+        },
+        semantic: {
+          danger: 'var(--danger)',
+          success: 'var(--success)',
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
