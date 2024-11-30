1fa3a1f56625e0d8b1600c82d6e02f71b0afea93
Giovani <giovani98@proton.me>
2024-11-30 10:49:13 -0300

    style: small change

--- src/components/base-button.tsx
+++ src/components/base-button.tsx
@@ -24,10 +24,10 @@ export default function BaseButton({
     'px-5 py-[18px] rounded-[10px] font-body text-action-md font-medium w-full transition-colors duration-100 ease-in-out flex items-center',
     variant === 'primary'
       ? disabled
-        ? 'bg-gray-100 text-gray-400'
+        ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
         : 'bg-orange-base text-shape-white hover:bg-orange-dark'
       : disabled
-      ? 'border-gray-100 text-gray-400'
+      ? 'border-gray-100 text-gray-400 cursor-not-allowed'
       : 'border-[1px] border-orange-base text-orange-base hover:border-orange-dark hover:text-orange-dark',
     {
       'justify-center': !iconLeft && !iconRight,
